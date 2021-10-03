7161065143465362d32454b5f70782c172bbc9e2
Gabriel Silva de Camargo <creeplays@gmail.com>
2021-10-03 19:34:05 -0300

    feat(UI): Ajustes de responsibilidade

--- src/app/game-modules/socket/socket.service.ts
+++ src/app/game-modules/socket/socket.service.ts
@@ -1,34 +1,37 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 
+import { io, Socket } from "socket.io-client";
 @Injectable({
     providedIn:'root'
 })
 
 export class SocketService {
-    socket : WebSocket;
+    socket : Socket;
     public _eventBehavior : BehaviorSubject<{key:number,value:string}> = new BehaviorSubject(null)
     public isConnected : boolean = false;
 
     socketReturn(){
-        this.socket = new WebSocket("ws://localhost:8080/game");
-        this.socket.onopen = (event) =>{
-            console.log("Iniciando Socket.");
-            this.isConnected = true;
-        }
-        this.socket.onmessage = (event : MessageEvent) =>{
-           let response = this.eventHandler(event.data);
-           this._eventBehavior.next(response);
-        }
-        this.socket.onerror = (event) => {
-            setTimeout(()=>{this.socketReturn()},1000)
-            console.log("Reconectando");
-        }
+        this.socket = io("http://localhost:3000");
+        debugger;
+        this.socket;
+        // this.socket. = (event) =>{
+        //     console.log("Iniciando Socket.");
+        //     this.isConnected = true;
+        // }
+        // this.socket.onmessage = (event : MessageEvent) =>{
+        //    let response = this.eventHandler(event.data);
+        //    this._eventBehavior.next(response);
+        // }
+        // this.socket.onerror = (event) => {
+        //     setTimeout(()=>{this.socketReturn()},1000)
+        //     console.log("Reconectando");
+        // }
     }
     
     socketMsg(key : string, value: string){
-        if(this.socket.readyState == this.socket.OPEN) this.socket.send(`${key}|${value}`);
-        else console.warn("Socket não está aberto");
+        //if(this.socket.readyState == this.socket.OPEN) this.socket.send(`${key}|${value}`);
+        //else console.warn("Socket não está aberto");
     }
 
     private eventHandler(message : string){
