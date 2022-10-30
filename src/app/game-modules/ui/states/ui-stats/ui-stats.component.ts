12528f4714b6f7ecb58ae00df03b583d4575f766
NandoPla <fernandopla8@gmail.com>
2022-10-30 18:45:31 -0300

    canvas stats

--- src/app/game-modules/ui/states/ui-stats/ui-stats.component.ts
+++ src/app/game-modules/ui/states/ui-stats/ui-stats.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UiStateControllerService, UiStatesEnum } from '../../ui-state-controller/ui-state-controller.service';
 
 @Component({
@@ -8,10 +8,27 @@ import { UiStateControllerService, UiStatesEnum } from '../../ui-state-controlle
 })
 export class UiStatsComponent implements OnInit {
 
+  @ViewChild("statscanvas", {static:true}) statsCanvas!: ElementRef<HTMLCanvasElement>
+  statsCanvasContext!: CanvasRenderingContext2D
+
   constructor(private uiState : UiStateControllerService) { }
 
   ngOnInit() {
+    this.statsCanvasContext = this.statsCanvas.nativeElement.getContext("2d")!;
+    this.draw();
   }
+
+  draw(){
+    this.statsCanvas.nativeElement.height = window.innerHeight;
+    this.statsCanvas.nativeElement.width = window.innerWidth;
+
+    this.statsCanvasContext.beginPath();
+    this.statsCanvasContext.moveTo(820,300);
+    this.statsCanvasContext.strokeStyle = "red";
+    this.statsCanvasContext.fillStyle = "blue";
+    this.statsCanvasContext.fillRect(50, 50, 50, 50);
+  }
+
 
   back(){
     this.uiState.changeState(UiStatesEnum.MENU);
